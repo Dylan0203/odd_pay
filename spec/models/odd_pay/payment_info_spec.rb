ee0fcd7330d823ceeab2b78d7213cd8fcd50f1b6
Dylan Lin <dylanmail0203@gmail.com>
2022-11-06 15:04:20 +0800

    ✅ add expired scope on PaymentInfo

--- spec/models/odd_pay/payment_info_spec.rb
+++ spec/models/odd_pay/payment_info_spec.rb
@@ -22,5 +22,45 @@ module OddPay
     it { should belong_to(:payment_method) }
     it { should have_many(:notifications) }
     it { should have_many(:payments) }
+
+    describe 'scopes' do
+      describe '.expired' do
+        subject { PaymentInfo.expired }
+
+        let(:invoice) { create :invoice, invoice_type: :subscription }
+        let(:payment_info) { create :payment_info, invoice: invoice, aasm_state: :paid }
+        let(:now) { Time.current }
+        let(:tomorrow) { now.next_day }
+        let!(:payment) { create :payment, payment_info: payment_info, ended_at: tomorrow }
+
+        shared_context 'payment_info is Not in the expired scope' do
+          it 'payment_info is Not in the expired scope' do
+            expect(subject).to match_array []
+          end
+        end
+
+        shared_context 'payment_info is in the expired scope' do
+          it 'payment_info is in the expired scope' do
+            expect(subject).to match_array [payment_info]
+          end
+        end
+
+        context 'if payment_info has vaild payment' do
+          include_context 'payment_info is Not in the expired scope'
+        end
+
+        context 'if payment_info has expired payment' do
+          before { payment.update! ended_at: now }
+
+          include_context 'payment_info is in the expired scope'
+
+          context 'multiple valid payments still get one payment_info' do
+            before { payment_info.payments.create!(ended_at: now) }
+
+            include_context 'payment_info is in the expired scope'
+          end
+        end
+      end
+    end
   end
 end
