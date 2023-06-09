357e44d0d2adf0627da2d2707a687984e5b1c45b
Dylan Lin <dylanmail0203@gmail.com>
2023-06-09 16:20:45 +0800

    ✨add default thanks for purchase page

---
+++ app/controllers/odd_pay/payment_infos/thanks_for_purchase_controller.rb
@@ -0,0 +1,23 @@
+# require_dependency "odd_pay/application_controller"
+
+module OddPay
+  class PaymentInfos::ThanksForPurchaseController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
+    def show
+      redirect_to main_app.root_path
+    end
+
+    def create
+      payment_info = PaymentInfo.find_by_hashid!(params[:payment_info_id])
+      notification = payment_info.notifications.new(
+        raw_data: request.POST,
+        reference: :payment_notify
+      )
+
+      @info = PaymentGatewayService.parse_notification(notification)
+
+      render :show
+    end
+  end
+end
